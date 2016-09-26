a378133b3096823b5e56793a294aac8e189b3953
Plonski <tplonski@villanova.edu>
2016-09-26 14:11:56 -0400

    First commit. Very early 	new file:   familytree.py

---
+++ familytree.py
@@ -0,0 +1,42 @@
+"""
+Name the project FamilyTree_Kim.py
+"""
+
+
+class Family_tree():
+
+    def __init__(self):
+        self.family_tree = {"siblings": [], "parents": [], "half-siblings": [], "cousins": []}
+
+class Family_member():
+
+    def __init__(self, name, relation):
+        self.name = name
+        self.relation = relation
+
+
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+        self.family_tree = Family_tree()
+
+    def add_parents(self, parent_one, parent_two):
+        pass
+
+    def add_sibling(self, sibling):
+        self.family_tree.family_tree['siblings'].append(sibling)
+
+    def list_relation(self, relation):
+        print(self.family_tree.family_tree[relation])
+
+
+x = Person("Thomas")
+Person("Mary")
+Person("jeff")
+
+
+
+
+x.add_sibling("jeff")
+x.list_relation("siblings")
