2a6f6341c230f1084db7456e562b0a072e166c6f
Plonski <tplonski@villanova.edu>
2016-10-01 03:15:00 -0400

    Added parents/children setup for the tree

--- familytree.py
+++ familytree.py
@@ -6,28 +6,57 @@ Name the project FamilyTree_Kim.py
 class Family_tree():
 
     def __init__(self):
-        self.family_tree = {"siblings": [], "parents": [], "half-siblings": [], "cousins": []}
-
-class Family_member():
-
-    def __init__(self, name, relation):
-        self.name = name
-        self.relation = relation
+        self.family_tree = {"siblings": [], "parents": [], "half-siblings": [], "cousins": [], "children": []}
 
 class Person:
 
     def __init__(self, name):
         self.name = name
-        self.family_tree = Family_tree()
+        self.family_tree = Family_tree().family_tree
 
     def add_parents(self, parent_one, parent_two):
+        self.family_tree['parents'].append(parent_one)
+        self.family_tree['parents'].append(parent_two)
+
+    def add_children(self, child):
+        self.family_tree['children'].append(child)
+
+    def add_siblings(self, parent_one, parent_two):
+        for child in parent_one.family_tree['children']:
+            if child not in self.family_tree['siblings'] and self.name is not child.name:
+                if child in parent_two.family_tree['children']:
+                    self.family_tree['siblings'].append(child)
+                    child.add_siblings(parent_one, parent_two)
+                else:
+                    self.family_tree['half-siblings'].append(child)
+                    if self not in child.family_tree['half-siblings']:
+                        child.family_tree['half-siblings'].append(self)
+
+        for child in parent_two.family_tree['children']:
+            if child not in self.family_tree['siblings'] and self.name is not child.name:
+                if child in parent_one.family_tree['children']:
+                    self.family_tree['siblings'].append(child)
+                    child.add_siblings(parent_one, parent_two)
+                else:
+                    self.family_tree['half-siblings'].append(child)
+                    if self not in child.family_tree['half-siblings']:
+                        child.family_tree['half-siblings'].append(self)
+
+    def add_cousins(self):
         pass
 
-    def add_sibling(self, sibling):
-        self.family_tree.family_tree['siblings'].append(sibling)
-
     def list_relation(self, relation):
-        print(self.family_tree.family_tree[relation])
+        #print(self.family_tree[relation])
+        for family_member in self.family_tree[relation]:
+            print(family_member.name)
+
+person_list = []
+
+def check_exists(person_name):
+    if person_name in person_list:
+        pass
+    else:
+        return Person(person_name)
 
 def fileRead():
     filename = input()
@@ -42,17 +71,39 @@ def fileRead():
 
 def main():
 
-    fileRead()
-
+    #fileRead()
+    a = Person("Rob")
     x = Person("Thomas")
-    Person("Mary")
-    Person("Jeff")
-    Person("Matt")
+    y = Person("Mary")
+    z = Person("Jeff")
+
+    half_p = Person("ewok")
+    half = Person("pete")
+
+    a.add_parents(y, z)
+    y.add_children(a)
+    z.add_children(a)
+    a.add_siblings(y, z)
+
+    x.add_parents(y, z)
+    y.add_children(x)
+    z.add_children(x)
+    a.add_siblings(y, z)
+
+    #a.list_relation("siblings")
+    #x.list_relation("siblings")
+
+    half.add_parents(half_p, y)
+    half_p.add_children(half)
+    y.add_children(half)
+    half.add_siblings(half_p, y)
+
+    #half.list_relation('half-siblings')
+    #half.list_relation('siblings')
+    a.list_relation('siblings')
 
 
-    x.add_sibling("Jeff")
-    x.add_sibling("Mary")
-    x.list_relation("siblings")
 
-if __name__ == '__main__':
-    main()
+main()
+#if __name__ == '__main__':
+#    main()
